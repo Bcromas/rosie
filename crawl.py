b71d9e9ce2061f63562f03d41fbb4ef9a7dd904e
Bryan <bcromas@gmail.com>
2020-05-06 22:25:54 -0400

    install & connect MariaDB

--- crawl.py
+++ crawl.py
@@ -1,4 +1,5 @@
 import praw
+import pymysql.cursors
 
 #initialize global vars, set to blank
 CLIENT_ID = CLIENT_SECRET = USERNAME = PASSWORD = ''
@@ -17,7 +18,34 @@ with open("secrets.txt") as secrets:
             PASSWORD = line_split[1].strip()
 #end getting global vars from secrets.txt
 
-reddit = praw.Reddit(client_id = CLIENT_ID, client_secret = CLIENT_SECRET, password = PASSWORD,user_agent = f"rosie by {USERNAME}", username = USERNAME)
+# reddit = praw.Reddit(client_id = CLIENT_ID, client_secret = CLIENT_SECRET, password = PASSWORD,user_agent = f"rosie by {USERNAME}", username = USERNAME)
+
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='bryan',
+                             password='1234',
+                             db='testdb',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    with connection.cursor() as cursor:
+        # Create a new record
+        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
+        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+
+    # connection is not autocommit by default. So you must commit to save
+    # your changes.
+    connection.commit()
+
+    with connection.cursor() as cursor:
+        # Read a single record
+        sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+        cursor.execute(sql, ('webmaster@python.org',))
+        result = cursor.fetchone()
+        print(result)
+finally:
+    connection.close()
 
 if __name__ == "__main__":
     print("in main")
