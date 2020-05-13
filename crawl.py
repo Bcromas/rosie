f40de3dea2d5ba72065565bfc6e57efd6b4b82a7
Bryan <bcromas@gmail.com>
2020-05-13 12:39:03 -0400

    add cache checking for Subreddits

--- crawl.py
+++ crawl.py
@@ -22,11 +22,61 @@ with open("secrets.txt") as secrets:
             DB_PASSWORD = line_split[1].strip()
 #end getting global vars from secrets.txt
 
-reddit = praw.Reddit(client_id = CLIENT_ID, client_secret = CLIENT_SECRET, password = PASSWORD,user_agent = f"rosie by {USERNAME}", username = USERNAME)
+REDDIT = praw.Reddit(
+    client_id = CLIENT_ID,
+    client_secret = CLIENT_SECRET,
+    password = PASSWORD,
+    user_agent = f"rosie by {USERNAME}", 
+    username = USERNAME
+    )
+
+def make_connection():
+    """
+    Establish connection to DB.
+
+    Args:
+        None
+    
+    Returns:
+        A connection instance.
+
+    """
+    this_connection = pymysql.connect(host = 'localhost',
+                                        user = DB_USER,
+                                        password = DB_PASSWORD,
+                                        db = 'testdb',
+                                        charset = 'utf8mb4',
+                                        cursorclass = pymysql.cursors.DictCursor)
+    
+    return this_connection
+
+def check_subreddit(name):
+    """
+    Check whether a Subreddit already exists in DB or not.
+
+    Args:
+        name - a string representing the name of a Subreddit
+
+    Returns:
+        Dictionary containing key-value pairs for Subreddit id and Subreddit display_name in DB.
+
+    """
+    try:
+        connection = make_connection()
+        cursor = connection.cursor()
+        statement = "SELECT * "
+        statement += "FROM Subreddit "
+        statement += f"WHERE display_name = '{name}'"
+        cursor.execute(statement)
+        result = cursor.fetchall()
+        subreddit = result[0]
+        return {"id":subreddit["id"], "display_name": subreddit["display_name"]}
+    except Exception as e:
+        return {}
 
 def insert_subreddits(some_subreddits):
     """
-    Attempts to insert Subreddits & details into DB. 
+    Insert Subreddits & details into DB.
 
     Args:
         some_subreddits - list of strings representing a Subreddit name.
@@ -35,23 +85,22 @@ def insert_subreddits(some_subreddits):
         None
     """
     for entry in some_subreddits:
-        try:
-            subreddit = reddit.subreddit(entry)
-            insertion = (subreddit.id, subreddit.name, subreddit.display_name, subreddit.subscribers, subreddit.user_is_banned)
-            connection = pymysql.connect(host = 'localhost',
-                                    user = DB_USER,
-                                    password = DB_PASSWORD,
-                                    db = 'testdb',
-                                    charset = 'utf8mb4',
-                                    cursorclass = pymysql.cursors.DictCursor)
-            cursor = connection.cursor()
-            statement = "INSERT INTO Subreddit "
-            statement += "VALUES (%s,%s,%s,%s,%s)"
-            cursor.execute(statement, insertion)
-            connection.commit()
-        except Exception as e:
-            print(f"Error in test insert: {e}.")
-            connection.close()
+        if check_subreddit(entry):
+            print(f"Subreddit '{entry}' already in DB.")
+            continue
+        else:
+            try:
+                subreddit = REDDIT.subreddit(entry)
+                insertion = (subreddit.id, subreddit.name, subreddit.display_name, subreddit.subscribers, subreddit.user_is_banned)
+                connection = make_connection()
+                cursor = connection.cursor()
+                statement = "INSERT INTO Subreddit "
+                statement += "VALUES (%s,%s,%s,%s,%s)"
+                cursor.execute(statement, insertion)
+                connection.commit()
+            except Exception as e:
+                print(f"Error in insert_subreddits: {e}.")
+                connection.close()
 
 def main():
     """
@@ -63,7 +112,7 @@ def main():
     Returns:
         None
     """
-    some_subreddits = ["worldnews", "offbeat", "news"]
+    some_subreddits = ["worldnews", "offbeat", "news", "AskReddit"]
     insert_subreddits(some_subreddits)
 
 if __name__ == "__main__":
