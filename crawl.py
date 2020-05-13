855af381a6a70a817325a8df59098f0076d18c78
Bryan <bcromas@gmail.com>
2020-05-12 22:28:49 -0400

    finish test crawl & populate DB

--- crawl.py
+++ crawl.py
@@ -22,27 +22,51 @@ with open("secrets.txt") as secrets:
             DB_PASSWORD = line_split[1].strip()
 #end getting global vars from secrets.txt
 
-# reddit = praw.Reddit(client_id = CLIENT_ID, client_secret = CLIENT_SECRET, password = PASSWORD,user_agent = f"rosie by {USERNAME}", username = USERNAME)
+reddit = praw.Reddit(client_id = CLIENT_ID, client_secret = CLIENT_SECRET, password = PASSWORD,user_agent = f"rosie by {USERNAME}", username = USERNAME)
 
-# Connect to the database
-connection = pymysql.connect(host = 'localhost',
-                             user = DB_USER,
-                             password = DB_PASSWORD,
-                             db = 'testdb',
-                             charset = 'utf8mb4',
-                             cursorclass = pymysql.cursors.DictCursor)
+def insert_subreddits(some_subreddits):
+    """
+    Attempts to insert Subreddits & details into DB. 
 
-try:
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-        cursor.execute(sql, ('webmaster@python.org',))
-        result = cursor.fetchone()
-        print(result)
-finally:
-    connection.close()
+    Args:
+        some_subreddits - list of strings representing a Subreddit name.
+
+    Returns:
+        None
+    """
+    for entry in some_subreddits:
+        try:
+            subreddit = reddit.subreddit(entry)
+            insertion = (subreddit.id, subreddit.name, subreddit.display_name, subreddit.subscribers, subreddit.user_is_banned)
+            connection = pymysql.connect(host = 'localhost',
+                                    user = DB_USER,
+                                    password = DB_PASSWORD,
+                                    db = 'testdb',
+                                    charset = 'utf8mb4',
+                                    cursorclass = pymysql.cursors.DictCursor)
+            cursor = connection.cursor()
+            statement = "INSERT INTO Subreddit "
+            statement += "VALUES (%s,%s,%s,%s,%s)"
+            cursor.execute(statement, insertion)
+            connection.commit()
+        except Exception as e:
+            print(f"Error in test insert: {e}.")
+            connection.close()
+
+def main():
+    """
+    Main function for control flow.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    some_subreddits = ["worldnews", "offbeat", "news"]
+    insert_subreddits(some_subreddits)
 
 if __name__ == "__main__":
-    print("in main")
-
+    main()
+    
 # FROM CL, can log into DB using 'mysql -u bryan -p testdb'
